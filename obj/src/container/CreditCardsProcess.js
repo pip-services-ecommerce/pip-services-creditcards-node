80c625aa2723a45924119fba547358afb7454f8d
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:13:59 +0300

    added DefaultRpcFactory

--- obj/src/container/CreditCardsProcess.js
+++ obj/src/container/CreditCardsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const CreditCardsServiceFactory_1 = require("../build/CreditCardsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class CreditCardsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("credit_cards", "Credit cards microservice");
         this._factories.add(new CreditCardsServiceFactory_1.CreditCardsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.CreditCardsProcess = CreditCardsProcess;
--- src/container/CreditCardsProcess.ts
+++ src/container/CreditCardsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { CreditCardsServiceFactory } from '../build/CreditCardsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class CreditCardsProcess extends ProcessContainer {
 
     public constructor() {
         super("credit_cards", "Credit cards microservice");
         this._factories.add(new CreditCardsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
