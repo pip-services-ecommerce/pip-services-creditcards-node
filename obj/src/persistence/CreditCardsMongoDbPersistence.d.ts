f09e02d920e21eb69f1a64b98e19ad99c972f807
pipdeveloper <pipdevs@gmail.com>
2019-05-22 11:43:51 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/CreditCardsMongoDbPersistence.d.ts
+++ obj/src/persistence/CreditCardsMongoDbPersistence.d.ts
@@ -6,6 +6,6 @@ import { CreditCardV1 } from '../data/version1/CreditCardV1';
 import { ICreditCardsPersistence } from './ICreditCardsPersistence';
 export declare class CreditCardsMongoDbPersistence extends IdentifiableMongoosePersistence<CreditCardV1, string> implements ICreditCardsPersistence {
     constructor();
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: (err: any, page: DataPage<CreditCardV1>) => void): void;
 }
